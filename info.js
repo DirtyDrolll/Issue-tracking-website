208b3bf252eb8b4dcd4f0d7d65473a3e309a6e83
Deneishin Prins <deneishinprins@gmail.com>
2026-04-20 22:09:18 +0200

    Create info.js with people and project data
    Added initial data for people and projects.

---
+++ info.js
@@ -0,0 +1,67 @@
+const people = [
+
+{
+    name: "Samantha",
+    surname: "Adams",
+    employeeId: 81200,
+    username: "SAdams01",
+    email: "AdamsS@gmail.com"
+
+},
+{
+    name: "Ronald",
+    surname: "Matthews",
+    employeeId: 81300,
+    username: "RMattews02",
+    email: "MatthewsR@gmail.com"
+    
+},
+{
+    name: "Steven",
+    surname: "Choi",
+    employeeId: 81400,
+    username: "SChoi03",
+    email: "ChoiS@gmail.com"
+    
+},
+{
+    name: "Ellen",
+    surname: "Swanepoel",
+    employeeId: 81500,
+    username: "ESwanepoel04",
+    email: "SwanepoelE@gmail.com"
+    
+},
+{
+    name: "Dennis",
+    surname: "Randall",
+    employeeId: 81600,
+    username: "DRandall05",
+    email: "RandallD@gmail.com"
+    
+},
+
+];
+
+const project = [
+    {
+        projectId: "p10012",
+        name: "Online Learning platform"
+
+    },
+    {
+        projectId: "p10112",
+        name: "Event management system"
+        
+    },
+    {
+        projectId: "p10212",
+        name: "Webiste Redesign"
+        
+    },
+    {
+        projectId: "p10312",
+        name: "Cloud File storage platform"
+        
+    }
+];
